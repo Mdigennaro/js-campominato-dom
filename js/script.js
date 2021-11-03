dd95d20f0290c3b36693e75cccf4bacdf4139734
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-03 13:20:19 +0100

    creazione bombe

--- js/script.js
+++ js/script.js
@@ -11,17 +11,21 @@ function play() {
   const grado = [100, 81, 49];
   const cellNumbers = grado [difficolta - 1];
   const cellRow = Math.sqrt(cellNumbers);
-
+  
+  const BOMB_NUMB = 16;
+  const bombs = createBombs();
+  console.log(bombs);
+  
   document.querySelector('main').innerHTML = "";
   campoDiGioco();
-
-  function campoDiGioco() {
   
+  function campoDiGioco() {
+    
     const campo = document.createElement('div');
     campo.className= 'container';
-
+    
     for (let i = 1; i <= cellNumbers; i++) {
-
+      
       const square = document.createElement('div');
       square.className = 'square';
       square.innerHTML = 
@@ -34,13 +38,34 @@ function play() {
       `;
       square.style.width = squareSize;
       square.style.height = squareSize;
+
+      square.addEventListener('click', function(event){
+        square.classList.add('active');
+        console.log(event.target.innerText);
+      })
       
       document.querySelector('main').append(campo);
       campo.append(square);
     }
   }
- 
+  
+  function createBombs() {
+    const bombs = [];
+
+    while (bombs.length < BOMB_NUMB) {
+      const bomb = getRandomNumb (1, cellNumbers);
+      if (!bombs.includes(bomb)) {
+        bombs.push(bomb);
+      }
+    }
+    
+    return bombs;
+  }
+
 }
 
 
+function getRandomNumb(min, max) {
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
 
